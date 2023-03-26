5e030b877986c1ba8f64ac1d82e796c7f409e38e
Ste7an <51531682+ste7anste7an@users.noreply.github.com>
2023-03-27 00:05:28 +0200

    docker files added

---
+++ docker/generate_certs.sh
@@ -0,0 +1,34 @@
+ generate_certs.sh
+#!/bin/bash
+
+# put your IP address here
+IP="<IP address server>"
+SUBJECT_CA="/C=NL/ST=Amsterdam/L=Amsterdam/O=freq/OU=CA/CN=$IP"
+SUBJECT_SERVER="/C=NL/ST=Stockholm/L=Amsterdam/O=freq/OU=Server/CN=$IP"
+SUBJECT_CLIENT="/C=NL/ST=Amsterdam/L=Amsterdam/O=freq/OU=Client/CN=$IP"
+
+function generate_CA () {
+   echo "$SUBJECT_CA"
+   openssl req -x509 -nodes -sha256 -newkey rsa:2048 -subj "$SUBJECT_CA"  -days
+365 -keyout ca.key -out ca.crt
+}
+
+function generate_server () {
+   echo "$SUBJECT_SERVER"
+   openssl req -nodes -sha256 -new -subj "$SUBJECT_SERVER" -keyout server.key -o
+ut server.csr
+   openssl x509 -req -sha256 -in server.csr -CA ca.crt -CAkey ca.key -CAcreatese
+rial -out server.crt -days 365
+}
+
+function generate_client () {
+   echo "$SUBJECT_CLIENT"
+   openssl req -new -nodes -sha256 -subj "$SUBJECT_CLIENT" -out client.csr -keyo
+ut client.key
+   openssl x509 -req -sha256 -in client.csr -CA ca.crt -CAkey ca.key -CAcreatese
+rial -out client.crt -days 365
+}
+
+generate_CA
+generate_server
+generate_client
